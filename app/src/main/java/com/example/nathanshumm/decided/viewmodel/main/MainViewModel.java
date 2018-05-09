dbc62427fec5b36868e77836680d71de8ed4acfa
CheckShumm <checkshumm@gmail.com>
2018-05-08 22:26:04 -0400

    Added navigation bar. Added navigation drawer. Added Guest Login.

---
+++ app/src/main/java/com/example/nathanshumm/decided/viewmodel/main/MainViewModel.java
@@ -0,0 +1,35 @@
+package com.example.nathanshumm.decided.viewmodel.main;
+
+import android.arch.lifecycle.ViewModel;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+
+import com.example.nathanshumm.decided.view.favorites.FavoritesFragment;
+import com.example.nathanshumm.decided.view.filter.FilterFragment;
+import com.example.nathanshumm.decided.view.home.HomeFragment;
+
+public class MainViewModel extends ViewModel {
+
+    private FavoritesFragment favoritesFragment;
+    private FilterFragment filterFragment;
+    private HomeFragment homeFragment;
+
+    public MainViewModel() {
+        this.favoritesFragment = new FavoritesFragment();
+        this.filterFragment = new FilterFragment();
+        this.homeFragment = new HomeFragment();
+    }
+
+    public FavoritesFragment getFavoritesFragment() {
+        return favoritesFragment;
+    }
+
+    public FilterFragment getFilterFragment() {
+        return filterFragment;
+    }
+
+    public HomeFragment getHomeFragment() {
+        return homeFragment;
+    }
+
+}
