7ec28784246ad09159f6b2e34f91b3d6f7b9faff
CheckShumm <checkshumm@gmail.com>
2018-05-10 17:58:09 -0400

    added google places api added json parser

---
+++ app/src/main/java/com/example/nathanshumm/decided/model/api/PlaceResponse.java
@@ -0,0 +1,37 @@
+package com.example.nathanshumm.decided.model.api;
+
+import android.util.Log;
+
+import com.google.android.gms.location.places.Place;
+
+import java.util.ArrayList;
+
+public class PlaceResponse {
+
+    private StringBuilder sbValue = new StringBuilder(sbMethod());
+    private PlacesTask placesTask = new PlacesTask();
+
+    private StringBuilder sbMethod() {
+
+        //use your current location here
+        double mLatitude = 45.5016890;
+        double mLongitude = -73.5672560;
+
+        StringBuilder sb = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
+        sb.append("location=" + mLatitude + "," + mLongitude);
+        sb.append("&radius=5000");
+        sb.append("&types=" + "restaurant");
+        sb.append("&sensor=true");
+        sb.append("&key=******* YOUR API KEY****************");
+
+        Log.d("Map", "api: " + sb.toString());
+
+        return sb;
+    }
+
+    public ArrayList<com.example.nathanshumm.decided.model.api.Place> getPlace(int index){
+        placesTask.execute(sbValue.toString());
+        return placesTask.getPlaceList();
+    }
+
+}
